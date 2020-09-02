181bd62928eab496bd1cf75f1b3b5006429377ca
Hiep <daihieptn97@gmail.com>
2020-09-02 12:41:37 +0700

    .

---
+++ sm3/GetWiFi.swift
@@ -0,0 +1,9 @@
+//
+//  GetWiFi.swift
+//  sm3
+//
+//  Created by Trần Hiệp on 9/2/20.
+//  Copyright © 2020 Trần Hiệp. All rights reserved.
+//
+
+import Foundation
---
+++ sm3/bridging.m
@@ -0,0 +1,9 @@
+//
+//  bridging.m
+//  sm3
+//
+//  Created by Trần Hiệp on 9/2/20.
+//  Copyright © 2020 Trần Hiệp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
